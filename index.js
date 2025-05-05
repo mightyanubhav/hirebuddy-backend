9060fa7859887c689432935fa16c45c2a89b0676
Anubhav Shukla <kumaranubhav691@gmail.com>
2025-05-05 18:26:45 +0530

    dev start

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hi there")
+const express = require('express')
+const app = express()
+const PORT = 7777
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+  
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+})
